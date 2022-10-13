83201d72c06bfe9c358becda0caac794ff719646
Nacho <nnnnnachooooo@gmail.com>
2022-10-13 16:07:25 +0200

    Restaurantes - pokemon API

--- client/src/components/Main/Restaurants/RestaurantCard/RestaurantCard.jsx
+++ client/src/components/Main/Restaurants/RestaurantCard/RestaurantCard.jsx
@@ -1,30 +1,16 @@
 import React from "react";
 
 
+
 const RestaurantCard = (props) => {
-  return (
-    <>
-      
-      <div >
-      <h5>Sustainable Restaurant:</h5>
-        Nombre: {props.item.name}
-        <br />
-        id: {props.item.id}
-        <br />
-        Categoría: {props.item.nametype}
-        <br />
-        Tfno: {props.item.recclass}
-        <br />
-        Reservas: {props.item.mass}
-        <br />
-        Ubicación: {props.item.fall}
-        <br />
-           
-
-      </div>
-
-    </>
-  );
+  const info = props.data;
+  
+  return(
+    <div>
+    <h4>{info.name}</h4>
+    <img src={info.image} alt="" />
+  </div>
+  )
 };
 
 
