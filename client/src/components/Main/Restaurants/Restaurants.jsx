4f6ec0ed21414914db1c1b8ad846a2e2a8d45597
Nacho <nnnnnachooooo@gmail.com>
2022-10-13 12:20:47 +0200

    Restaurants.jsx a falta de restauranCard y testeo

--- client/src/components/Main/Restaurants/Restaurants.jsx
+++ client/src/components/Main/Restaurants/Restaurants.jsx
@@ -1,22 +1,56 @@
-import React, { Component,useContext } from "react";
-import { useEffect } from "react";
-import { checkUserContext } from "../../../context/checkUserContext";
+import React, { Component, useState, useEffect } from "react";
+//import { checkUserContext, useContext } from "../../../context/checkUserContext";
 import { useNavigate } from "react-router-dom";
 import {Link} from 'react-router-dom';
+import axios from 'axios';
+import RestaurantCard from './RestaurantCard'
+
+
 const Restaurants = ()=>{
-  const {userCheck,setUserCheck} = useContext(checkUserContext);
+
+  const [values, setValues] = useState({
+    designation: '',
+    discovery_date: '',
+    h_mag: '',
+    moid_au: '',
+    q_au_1: '',
+    q_au_2: '',
+    period_yr: '',
+    i_deg: '',
+    pha: '',
+    orbit_class: ''
+  });
+
+  //const {userCheck,setUserCheck} = useContext(checkUserContext);
   const navigate = useNavigate();
+  const [dataRestaurants, setDataRestaurants] = useState([])
+  const [currentItems] = useState(null);
+
+
+  async function loadDataRestaurants() {
+    let res = await axios.get("https://nasaapinacholopez.herokuapp.com/api/astronomy/neas?to=2022")
+    let data = res.data
+    setDataRestaurants(data)
+    console.log(dataRestaurants);
+  }
 
   useEffect(()=>{
-    console.log(userCheck);
-    if(userCheck===""){
-      navigate("/");
+    loadDataRestaurants();
+    // console.log(userCheck);
+    // if(userCheck===""){
+    //   navigate("/");
     }
-  },[])
+  ,[])
 
   return(
     <>
       <h2>Restaurants</h2>
+      <>
+        {currentItems &&
+          currentItems.map((item, i) => (
+            <RestaurantCard key={i} item={item}/>
+          ))}
+      </>
       <Link to="/">Volver</Link>
     </>
   )
