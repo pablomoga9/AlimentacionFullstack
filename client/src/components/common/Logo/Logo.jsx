a7b73610dc2f5d512437081ea7930a821b3d459d
RMoreno207 <107397146+RMoreno207@users.noreply.github.com>
2022-10-14 20:32:19 +0200

    refactoring logo link

--- client/src/components/common/Logo/Logo.jsx
+++ client/src/components/common/Logo/Logo.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Logo = () => {
   return (
     <div>
-      <Link to={"/"}><img style={{ width: "200px" }} src="https://cdn.pixabay.com/photo/2016/12/17/18/12/logo-1914020_1280.png" alt="Logo" />
+      <Link to={"/home"}><img style={{ width: "200px" }} src="https://cdn.pixabay.com/photo/2016/12/17/18/12/logo-1914020_1280.png" alt="Logo" />
       </Link>
     </div>
   );
