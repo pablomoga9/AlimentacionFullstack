b631ce73537c803b9b4e434535e359ea1e1645df
pablomoga9 <moyagarcia99@gmail.com>
2022-10-17 14:13:04 +0200

    portada timeout

--- client/src/components/Main/Login/Login.jsx
+++ client/src/components/Main/Login/Login.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState, useContext } from "react";
 import { useForm } from 'react-hook-form';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-
+import LogoCircle from '../../../assets/img/logoCircle.png'
 import { useNavigate } from 'react-router-dom';
 import jwtDecode from 'jwt-decode';
 import { checkUserContext } from '../../../context/checkUserContext';
@@ -14,9 +14,22 @@ const Login = () => {
   const { userCheck, setUserCheck } = useContext(checkUserContext);
   const { userDetails } = useContext(checkUserContext);
   const { checkUser } = useContext(checkUserContext);
-
+  const [time,setTime] = useState(false);
 
   useEffect(() => {
+    if(userCheck===null){
+      setTime(false)
+    }
+    else{
+      setTime(true);
+    }
+
+    if(time===false){
+      const timeout = setTimeout(()=>{
+       setTime(true)
+      },2000);
+      return ()=>clearTimeout(timeout)
+    }
   }, [])
 
 
@@ -46,7 +59,8 @@ const Login = () => {
 
   return (
     <>
-      <Logo />
+     {time===false?<div><h1 className="logoText">K'm<img className="logoImg" src={LogoCircle}></img>on</h1></div>:<div>
+     <Logo />
       <div>
         {userCheck == null ? <h3>Inicia sesión</h3> : <h3>Has iniciado sesión como: </h3>}
         {userCheck == null ? <form onSubmit={handleSubmit(onSubmit)}>
@@ -75,6 +89,7 @@ const Login = () => {
           ¿Has olvidado tu contraseña?
         </span></Link>
       </div>
+      </div>}
     </>
   )
 }
